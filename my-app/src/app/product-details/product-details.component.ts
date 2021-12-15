a1a1342c5ef412eee26b4dd0336ed8fc15cf7191
joanri20 <dairo.rivas@udea.edu.co>
2021-12-14 20:50:24 -0500

    Manejo de datos, carrito de compras con precios de productos

--- my-app/src/app/product-details/product-details.component.ts
+++ my-app/src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -8,9 +9,14 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Esta producto ha sido agregado al carrito');
+  }
+
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
     // First get the product id from the current route.
